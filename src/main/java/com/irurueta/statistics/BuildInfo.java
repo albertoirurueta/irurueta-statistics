b969010024810c460afe7a13817b7b62f6a261de
Alberto Irurueta Carro <albertoirurueta@gmail.com>
2017-06-26 23:47:44 +0200

    Initial commit

--- src/main/java/com/irurueta/statistics/BuildInfo.java
+++ src/main/java/com/irurueta/statistics/BuildInfo.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.ref.SoftReference;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Properties;
