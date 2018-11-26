41854b1785aa5a7832db91c5a41be8fbe361a604
Alberto Irurueta <alberto@irurueta.com>
2018-11-27 00:26:08 +0100

    Code quality improvements

--- src/main/java/com/irurueta/statistics/NormalDist.java
+++ src/main/java/com/irurueta/statistics/NormalDist.java
@@ -172,8 +172,7 @@ public class NormalDist {
      */
     public static double cdf(double x, double mu, double sig) {
         if (sig <= 0.0) {
-            throw new IllegalArgumentException(
-                    "standard deviation must be greater than zero");
+            throw new IllegalArgumentException();
         }
 
         return internalCdf(x, mu, sig);
