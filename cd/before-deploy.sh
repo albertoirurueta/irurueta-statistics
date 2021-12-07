26ca45a2a86e788d7acf0d6599a466640d33d17b
Alberto Irurueta <alberto@irurueta.com>
2021-12-07 23:21:19 +0100

    1.0.0 Release

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -3,3 +3,7 @@ if [ "$TRAVIS_BRANCH" = 'develop' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; th
     openssl aes-256-cbc -K $encrypted_2c93df70a840_key -iv $encrypted_2c93df70a840_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    openssl aes-256-cbc -K $encrypted_2c93df70a840_key -iv $encrypted_2c93df70a840_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    gpg --fast-import cd/codesigning.asc
+fi
